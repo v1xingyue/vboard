921d15d393a80c1ce56d0dbe06b5d9968ef93708
xingyue <xingyue@xsec.com>
2018-07-09 15:20:34 +0800

    simple board

--- js/app.js
+++ js/app.js
@@ -1,9 +1,38 @@
-var canvas = document.getElementById('myCanvas');
-paper.setup(canvas);
-var path = new paper.Path();
-path.strokeColor = 'red';
-var start = new paper.Point(100, 100);
-path.moveTo(start);
-path.lineTo(start.add([ 200, -50 ]));
-paper.view.draw();
+paper.install(window);
 
+var canvas = document.getElementById('mainCanvas');
+paper.setup(canvas);
+
+
+var blocks = [];
+var colors = ["red","green","blue"];
+for(var i = 0; i < 24 ; i++){
+    blocks[i] = new Path.Rectangle([i * 50 + 10,200],[30,30]);
+    blocks[i].strokeColor = colors[ i % 3]; 
+
+    blocks[i+24] = new Path.Rectangle([i * 50 + 10,60],[30,30]);
+    blocks[i+24].strokeColor = colors[ i % 3]; 
+}
+
+var text = new PointText(new Point(450, 150));
+text.fillColor = "gray";
+text.content = "Hello , This Is Paper.js World!";
+text.fontSize = "24";
+
+var textUnderLine = new Path([450,160],[780,160]);
+textUnderLine.strokeColor = "black";
+textUnderLine.strokeWidth = 3;
+
+
+view.draw();
+view.onFrame = function(event){
+    for(var i = 0; i < 48; i++){
+        if(i%2) {
+            blocks[i].rotate(-1);
+        } else{
+            blocks[i].rotate(1);
+        }
+    }
+
+
+}
